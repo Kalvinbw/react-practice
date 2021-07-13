2fa9341a2a8670808752cc5972c34901673cc44f
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-07-13 16:03:31 -0600

    Begin matatu creation

---
+++ matatu/src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div><p>Hello World!</p></div>
+        );
+    }
+}
+
+export default Header;
