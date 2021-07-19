5f0849ffa0da0b3db2efb51c9f8e1a352bc0607d
Kalvin Wettstein <kalvinwettstein@gmail.com>
2021-07-19 09:15:18 -0600

    Moved deck creation to deck.js

---
+++ matatu/server/deck.js
@@ -0,0 +1,112 @@
+class Card {
+    constructor(ID, sui, nam, num, abil, v) {
+        this.id = ID;
+        this.suit = sui;
+        this.name = nam;
+        this.number = num;
+        this.ability = abil;
+        this.value = v
+    }
+}
+
+class Deck {
+    constructor(cardArray) {
+        this.cards = cardArray;
+        this.discardPile = [];
+        this.drawPile = [];
+    }
+
+    shuffle() {
+
+    }
+
+    split() {
+
+    }
+
+    discard(c) {
+        for (let i = 0; i < c.length; i++) {
+            this.discardPile.push(c[i]);
+            this.cards.splice();
+        }
+    }
+}
+
+function makeDeck() {
+    let suits = ['Heart', 'Spade', 'Club', 'Diamond'];
+    let names = ['Ace', 'Jack', 'Queen', 'King', 'Joker'];
+    let ability = ['Draw 2', 'Draw 4', 'Draw 5', 'Skip Turn', 'Wild'];
+    let values = [25, 50, 100];
+
+    let cardAr = [];
+    //create all the cards
+    for(let i=0; i<suits.length; i++) {
+        let sui = suits[i];
+        let num;
+        let val;
+        let abil;
+        let nam;
+        for(let j=1; j<14; j++) {
+            let ID = "" + i + j;
+            num = j;
+            switch(num) {
+                case 1:
+                    nam = names[0];
+                    val = values[0];
+                    abil = ability[3];
+                    break;
+                case 2:
+                    nam = num;
+                    val = num;
+                    abil = ability[0];
+                    break;
+                case 8:
+                    nam = num;
+                    val = values[1];
+                    abil = ability[4];
+                    break;
+                case 10:
+                    nam = num;
+                    val = num;
+                    abil = ability[1];
+                    break;
+                case 11:
+                    nam = names[1];
+                    val = num;
+                    abil = null;
+                    break;
+                case 12:
+                    nam = names[2];
+                    val = num;
+                    abil = null;
+                    break;
+                case 13:
+                    nam = names[3];
+                    val = num;
+                    abil = null;
+                    break;
+                default:
+                    nam = num;
+                    val = num;
+                    abil = null;
+                    break;
+            } //end switch
+            let c = new Card(ID, sui, nam, num, abil, val);
+            cardAr.push(c);
+        }
+    }
+
+    //create the joker cards
+    for(let i = 0; i < 2; i++) {
+        let c = new Card(i, 'Joker', names[4], 0, ability[2], values[2]);
+        cardAr.push(c);
+    }
+
+    let deck = new Deck(cardAr);
+    return [deck, cardAr];
+}
+
+let d = makeDeck();
+d = d[1];
+
+module.exports = d;
